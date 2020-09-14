1e690e6762253a2c4bb6b1c9173fd040670be01d
Igboanugwo Collins <munisco12@gmail.com>
2020-09-14 16:38:52 +0100

    Building the header section

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
       <h2>Welcome to amazon clone</h2>
+
+      <Switch>
+        <Route path= '/checkout'><h1>Checkout</h1></Route>
+        <Route path= '/shop'><h1>Shop</h1></Route>
+        <Route path= '/'><h1>Home</h1></Route>
+      </Switch>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './sass/main.scss';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './sass/main.scss'
+import App from './containers/App'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
