88bbb15be2e69958235dcf36adf2b95466132958
Igboanugwo Collins <munisco12@gmail.com>
2020-09-14 15:41:22 +0100

    Setting up react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/main.scss';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
