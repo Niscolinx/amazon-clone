fd294ef34c4c542d79b423cad40035bf0eaaff22
Igboanugwo Collins <munisco12@gmail.com>
2020-09-15 19:48:24 +0100

    Styling the select option

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket'
 
 function Header() {
     return (
@@ -14,30 +15,37 @@ function Header() {
             </Link>
 
             <div className='header__searchBox'>
-                <input type='text' className='header__searchInput'/>
-                <SearchIcon />
+                <select className='header__searchBox--select'>
+                    <option>1</option>
+                    <option>1</option>
+                    <option>1</option>
+                </select>
+                <input type='text' className='header__searchBox--input' />
+                <SearchIcon className='header__searchBox--icon' />
             </div>
 
-            <div className="header__nav">
-
-                <NavLink to='/'>
-                    <div className="header__nav--options">
-                        <span>Hello, sign in</span>
-                        <span>Acounts & Lists</span> 
+            <div className='header__nav'>
+                <NavLink to='/' className='header__link'>
+                    <div className='header__options'>
+                        <span className='header__options-light'>
+                            Hello, Sign in
+                        </span>
+                        <span className='header__options-bold'>
+                            Acounts & Lists
+                        </span>
                     </div>
                 </NavLink>
 
-                <NavLink to='/'>
-                    <div className="header__nav--options">
-                        <span>Returns</span>
-                        <span>& Order</span> 
+                <NavLink to='/' className='header__link'>
+                    <div className='header__options'>
+                        <span className='header__options-light'>Returns</span>
+                        <span className='header__options-bold'>& Order</span>
                     </div>
                 </NavLink>
-                
-                <NavLink to='/'>
-                    <div className="header__nav--options">
-                        <span>cart logo</span>
-                        <span>Cart</span> 
+
+                <NavLink to='/' className='header__link'>
+                    <div className='header__options--cart'>
+                        <ShoppingBasket /> <span className='header__options--cart-count'></span> Cart
                     </div>
                 </NavLink>
             </div>
