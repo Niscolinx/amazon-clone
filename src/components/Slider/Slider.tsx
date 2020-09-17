554d203a53a7f86d2cbe8af60640e93d3ed9f8eb
Igboanugwo Collins <munisco12@gmail.com>
2020-09-18 00:27:16 +0100

    styling the slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 function Slider() {
 
+    const getWidth = () => window.innerWidth
+    interface myStyles {
+        transform: string;
+        transition: string;
+    }
+
+    const customStyles: myStyles = {
+        transform: 'translateX(-432px)',
+        transition: 'all ease-in 4s'
+    }
+
+    console.log('the inner width is ', getWidth())
+
     const images = [
         'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg',
         'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg',
@@ -10,7 +23,7 @@ function Slider() {
     ]
 
     return (
-        <div className='slider'>
+        <div className='slider' style={customStyles}>
             hello world
         </div>
     )
