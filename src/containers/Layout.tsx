0a3e50acb9dead564de8cf1caa9f6abf2664a142
Igboanugwo Collins <munisco12@gmail.com>
2020-09-18 07:10:51 +0100

    Building the slider

--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -2,33 +2,6 @@ import React from 'react'
 import Slider from '../components/Slider/Slider'
 
 function Layout(props: any) {
-    // let [count, setCount] = useState(0)
-    // const [initialNumber, setInitialNumber] = useState(0)
-
-    // const carousel = (c: number) => {
-    //     console.log('the inner count', c)
-    //     if (c >= images.length) {
-    //         c = 0
-    //     }
-    //     if (c < 0) {
-    //         c = images.length - 1
-    //     }
-
-    //     return images[c]
-    // }
-
-    // let num: number = 0
-    // setInterval(() => {
-    //     num++
-
-    //     if(num >= images.length){
-    //         num = 0
-    //     }
-    //     console.log('setinterval', num)
-
-    //     setInitialNumber(num)
-    //    // setCount(num)
-    // }, 5000)
 
     return (
         <>
