891ff19cc80d7764ca5705f46a17e3780837496b
Igboanugwo Collins <munisco12@gmail.com>
2020-09-24 15:11:20 +0100

    Went back to the slider i built

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import {Switch, Route} from 'react-router-dom'
 
 import Header from '../components/Header/Header'
